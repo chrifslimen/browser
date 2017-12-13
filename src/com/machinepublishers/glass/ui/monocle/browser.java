ebb8f03b7ff77379ec58de12082e61bf8ddb03b8
maha ben ghzala <Dell@192.168.1.103>
2017-12-13 21:57:08 +0100

    version 2.1

--- src/com/machinepublishers/glass/ui/monocle/browser.java
+++ src/com/machinepublishers/glass/ui/monocle/browser.java
@@ -1,12 +1,35 @@
 package com.machinepublishers.glass.ui.monocle;
 
 public class browser {
+	public String  nom2="maha ben";
+	public String  prenom2="ghzala";
 		public String  nom="chrif";
 		public String  prenom="slimen";
 		public String  version="1.0";
 		public String  nomprojet="browser";
 		public String getNom() {
 			return nom;
+		}
+		public browser(String nom2, String prenom2, String nom, String prenom, String version, String nomprojet) {
+			super();
+			this.nom2 = nom2;
+			this.prenom2 = prenom2;
+			this.nom = nom;
+			this.prenom = prenom;
+			this.version = version;
+			this.nomprojet = nomprojet;
+		}
+		public String getNom2() {
+			return nom2;
+		}
+		public void setNom2(String nom2) {
+			this.nom2 = nom2;
+		}
+		public String getPrenom2() {
+			return prenom2;
+		}
+		public void setPrenom2(String prenom2) {
+			this.prenom2 = prenom2;
 		}
 		public void setNom(String nom) {
 			this.nom = nom;
