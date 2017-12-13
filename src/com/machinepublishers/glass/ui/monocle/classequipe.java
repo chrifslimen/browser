dc296bcb9017bd9a94ad52a69effbc751e478993
chrif <Dell@192.168.1.103>
2017-12-13 22:01:05 +0100

    version 4.1.0

---
+++ src/com/machinepublishers/glass/ui/monocle/classequipe.java
@@ -0,0 +1,24 @@
+package com.machinepublishers.glass.ui.monocle;
+
+public class classequipe {
+	public String clas="LAI 3 ISIMA";
+	public String equipe="chrif slimen, wifek racheh , maha ben ghzala";
+	public String getClas() {
+		return clas;
+	}
+	public void setClas(String clas) {
+		this.clas = clas;
+	}
+	public String getEquipe() {
+		return equipe;
+	}
+	public void setEquipe(String equipe) {
+		this.equipe = equipe;
+	}
+	public classequipe(String clas, String equipe) {
+		super();
+		this.clas = clas;
+		this.equipe = equipe;
+	}
+	
+}
